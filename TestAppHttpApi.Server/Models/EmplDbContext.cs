1ec81aa85a0ba63c2d7044128ee17a3b0a5f4452
Nikita Galyga <nikitagalyga@gmail.com>
2024-11-04 21:50:46 +1000

    add Serv

---
+++ TestAppHttpApi.Server/Models/EmplDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestAppHttpApi.Server.Models
+{
+    public class EmplDbContext : DbContext
+    {
+        public EmplDbContext(DbContextOptions<EmplDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
--- TestAppHttpApi.Server/Models/Employee.cs
+++ TestAppHttpApi.Server/Models/Employee.cs
@@ -11,10 +11,6 @@ namespace TestAppHttpApi.Server.Models
         public string LastName { get; set; }
         public string PositionCode { get; set; }
         public string DepartmentCode { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string Email { get; set; }
-        public string WorkPhone { get; set; }
-        public int Gender { get; set; }
         public bool IsHidden { get; set; }
         public DateTime LastModified { get; set; }
     }
